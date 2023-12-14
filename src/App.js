1c6a6e7235558d7256163edbb54705e62203119a
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-12-14 01:38:19 +0000

    Add useEffect hook and initialize movies state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useEffect } from 'react';
 import './App.css';
 import { Auth } from './components/auth';
+import { db } from "./config/firebase";
 
 function App() {
+  const [movies,setMovies]=useState([]);
+  useEffect(()=>{
+    const getMovies = async () => {
+
+    }
+  },[]);
+  
   return (
     <div className="App">
       <Auth/>
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBwRuZ8F4tkbweISSD86DsYlUdXNn9rJ1A",
@@ -8,8 +9,9 @@ const firebaseConfig = {
   storageBucket: "crudeauth-61035.appspot.com",
   messagingSenderId: "272884653372",
   appId: "1:272884653372:web:20648aaf27cefb823a5239",
-  measurementId: "G-GVL249V6B7"
+  measurementId: "G-GVL249V6B7",
 };
 
 const app = initializeApp(firebaseConfig);
- export const auth = getAuth(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
