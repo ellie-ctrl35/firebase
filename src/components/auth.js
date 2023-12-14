c5860c850ea3c366d9f7e4e8b0fd056d53701457
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-12-14 01:08:03 +0000

    Add email and password state variables and implement sign-in functionality

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,8 +1,20 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { auth } from "../../config/firebase";
+
 export const Auth = () => {
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const signIn = async () => {
+      await createUserWithEmailAndPassword(auth,email,password)
+    };
     return (
         <div>
-         <input placeholder="Email.."/>
-         <input placeholder="Password.."/>
+         <input placeholder="Email.."
+         onChange={(e)=>setEmail(e.target.value)}/>
+         <input placeholder="Password.."
+         onChange={(e)=>setPassword(e.target.value)}/>
+         <button onClick={signIn}>Sign In</button>
         </div>
     );
 };
