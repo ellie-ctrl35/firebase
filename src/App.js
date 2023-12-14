bf1e07371d78b5ea12b68667d69a68effb5fff18
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-12-14 00:59:49 +0000

    Refactor App.js to use Auth component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { Auth } from './components/auth';
 
 function App() {
   return (
     <div className="App">
-      <h1>My React App</h1>
+      <Auth/>
     </div>
   );
 }
